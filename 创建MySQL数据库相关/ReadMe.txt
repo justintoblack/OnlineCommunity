44d4856d808f0e5069f21ad79c969d4921b8e66f
曾杰航 <870582311@qq.com>
2022-04-29 17:53:48 +0800

    添加了创建MySQL数据库表的.sql文件和帮助

---
+++ 创建MySQL数据库相关/ReadMe.txt
@@ -0,0 +1,8 @@
+创建数据库表帮助
+
+1.Win + R 输入cmd并运行
+2.mysql -u root -p
+3.输入密码
+4.输入source 文件路径
+如：source F:/online_community_dump.sql
+5.上述文件仅有创建数据库及表，若要删除库请手动输入DROP DATABASE online_community;
---
+++ 创建MySQL数据库相关/online_community_dump.sql
@@ -0,0 +1,98 @@
+CREATE DATABASE IF NOT EXISTS online_community;
+USE online_community;
+
+CREATE TABLE IF NOT EXISTS `user`
+(
+    `uid`  INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `upwd` VARCHAR(20)  NOT NULL
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `moment`
+(
+    `mid`           INT UNSIGNED                 NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `uid`           INT UNSIGNED                 NOT NULL,
+    `mcontent`      varchar(255) CHARSET utf8mb4 NOT NULL,
+    `mpicurl`       varchar(255),
+    `mtime`         DATETIME                     NOT NULL,
+    `mlikecount`    int,
+    `mcommentcount` int,
+    `mrepostcount`  int
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+ALTER TABLE `moment`
+    ADD CONSTRAINT fk_moment_user FOREIGN KEY (`uid`) REFERENCES `user` (uid);
+
+CREATE TABLE IF NOT EXISTS `comment`
+(
+    `cid`        INT UNSIGNED                 NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `ccontent`   varchar(255) CHARSET utf8mb4 NOT NULL,
+    `mid`        INT UNSIGNED                 NOT NULL,
+    `cuid`       INT UNSIGNED                 NOT NULL,
+    `muid`       INT UNSIGNED                 NOT NULL,
+    `clikecount` INT UNSIGNED                 NOT NULL,
+    `ctime`      DATETIME                     NOT NULL DEFAULT NOW()
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+ALTER TABLE `comment`
+    ADD CONSTRAINT fk_comment_moment FOREIGN KEY (`mid`) REFERENCES `moment` (`mid`);
+ALTER TABLE `comment`
+    ADD CONSTRAINT fk_comment_user FOREIGN KEY (`cuid`) REFERENCES `user` (`uid`);
+
+CREATE TABLE IF NOT EXISTS `like`
+(
+    `lid`  INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `mid`  INT UNSIGNED NOT NULL,
+    `luid` INT UNSIGNED NOT NULL,
+    `muid` INT UNSIGNED NOT NULL
+) ENGINE = InnoDB;
+ALTER TABLE `like`
+    ADD CONSTRAINT fk_like_moment FOREIGN KEY (`mid`) REFERENCES `moment` (`mid`);
+ALTER TABLE `like`
+    ADD CONSTRAINT fk_like_user FOREIGN KEY (`luid`) REFERENCES `user` (`uid`);
+
+CREATE TABLE IF NOT EXISTS `repost`
+(
+    `rid`   INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `mid`   INT UNSIGNED NOT NULL,
+    `ruid`  INT UNSIGNED NOT NULL,
+    `muid`  INT UNSIGNED NOT NULL,
+    `rtime` DATETIME     NOT NULL DEFAULT NOW()
+) ENGINE = InnoDB;
+ALTER TABLE `repost`
+    ADD CONSTRAINT fk_repost_moment FOREIGN KEY (`mid`) REFERENCES `moment` (`mid`);
+ALTER TABLE `repost`
+    ADD CONSTRAINT fk_repost_user FOREIGN KEY (`ruid`) REFERENCES `user` (`uid`);
+
+CREATE TABLE IF NOT EXISTS `star`
+(
+    `sid`   INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `mid`   INT UNSIGNED NOT NULL,
+    `suid`  INT UNSIGNED NOT NULL,
+    `stime` DATETIME     NOT NULL DEFAULT NOW()
+) ENGINE = InnoDB;
+ALTER TABLE `star`
+    ADD CONSTRAINT fk_star_moment FOREIGN KEY (`mid`) REFERENCES `moment` (`mid`);
+ALTER TABLE `star`
+    ADD CONSTRAINT fk_star_user FOREIGN KEY (`suid`) REFERENCES `user` (`uid`);
+
+CREATE TABLE IF NOT EXISTS `user_info`
+(
+    `uid`          INT UNSIGNED                NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `unickname`    VARCHAR(30) CHARSET utf8mb4 NOT NULL,
+    `urealname`    VARCHAR(30) CHARSET utf8mb4,
+    `uavatarurl`   VARCHAR(255),
+    `uabout`       VARCHAR(255) CHARSET utf8mb4,
+    `ufollowing`   int,
+    `ufollowers`   int,
+    `umomentcount` int,
+    `ulikecount`   int,
+    `ustarcount`   int,
+    `ubirthday`    DATE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+ALTER TABLE `user_info`
+    ADD CONSTRAINT fk_userinfo_user FOREIGN KEY (`uid`) REFERENCES `user` (`uid`);
+
+
+
+
