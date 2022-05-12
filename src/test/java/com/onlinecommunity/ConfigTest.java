28ae0ad9a16f501d5601229a8d13bb395e3a9f9b
justintoblack <870582311@qq.com>
2022-05-12 17:18:30 +0800

    添加上传文件限制的配置

---
+++ src/test/java/com/onlinecommunity/ConfigTest.java
@@ -0,0 +1,19 @@
+package com.onlinecommunity;
+
+import com.onlinecommunity.config.UploadConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ConfigTest {
+    @Autowired
+    UploadConfig uplaodConfig;
+
+    @Test
+    public void testConfig(){
+        System.out.println("uplaodConfig.getPath() = " + uplaodConfig.getPath());
+        System.out.println("uplaodConfig.getPictureMaxSize() = " + uplaodConfig.getPictureMaxSize());
+        
+    }
+}
