2b32ebf3e7c212f3e8db82ad5d575692c77c1037
曾杰航 <870582311@qq.com>
2022-05-31 12:01:48 +0800

    修改sql建表文件

--- MySQL/online_community_dump.sql
+++ MySQL/online_community_dump.sql
@@ -5,19 +5,23 @@ CREATE TABLE IF NOT EXISTS `user`
 (
     `uid`      INT UNSIGNED                NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `username` VARCHAR(30) CHARSET utf8mb4 NOT NULL UNIQUE,
-    `password` VARCHAR(20)                 NOT NULL
+    `password` VARCHAR(20)                 NOT NULL,
+    `phone`    VARCHAR(30),
+    `email`    VARCHAR(50)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `moment`
 (
-    `mid`           INT UNSIGNED                 NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `moment_id`     INT UNSIGNED                 NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `uid`           INT UNSIGNED                 NOT NULL,
-    `mcontent`      varchar(255) CHARSET utf8mb4 NOT NULL,
-    `mtime`         DATETIME                     NOT NULL DEFAULT NOW(),
-    `picturescount` INT,
-    `mlikecount`    INT,
-    `mcommentcount` INT,
-    `mrepostcount`  INT
+    `content`       varchar(255) CHARSET utf8mb4 NOT NULL,
+    `moment_time`   DATETIME                     NOT NULL DEFAULT NOW(),
+    `is_active`     tinyint(1),
+    `deleted_at`    DATETIME,
+    `picture_count` INT,
+    `like_count`    INT,
+    `comment_count` INT,
+    `repost_count`  INT
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 ALTER TABLE `moment`
@@ -25,79 +29,82 @@ ALTER TABLE `moment`
 
 CREATE TABLE IF NOT EXISTS `picture`
 (
-    `mid`   INT UNSIGNED NOT NULL,
-    `index` INT          NOT NULL,
-    `url`   VARCHAR(255),
-    PRIMARY KEY (`mid`, `index`),
-    CONSTRAINT `fk_picture_moment` FOREIGN KEY (`mid`) REFERENCES `moment` (mid)
+    `moment_id` INT UNSIGNED NOT NULL,
+    `idx` INT          NOT NULL,
+    `url` VARCHAR(255),
+    PRIMARY KEY (`moment_id`, `idx`),
+    CONSTRAINT `fk_picture_moment` FOREIGN KEY (`moment_id`) REFERENCES `moment` (moment_id)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `comment`
 (
-    `cid`        INT UNSIGNED                 NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `ccontent`   varchar(255) CHARSET utf8mb4 NOT NULL,
-    `mid`        INT UNSIGNED                 NOT NULL,
-    `cuid`       INT UNSIGNED                 NOT NULL,
-    `muid`       INT UNSIGNED                 NOT NULL,
-    `clikecount` INT UNSIGNED                 NOT NULL,
-    `ctime`      DATETIME                     NOT NULL DEFAULT NOW()
+    `comment_id`   INT UNSIGNED                 NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `content`      varchar(255) CHARSET utf8mb4 NOT NULL,
+    `moment_id`    INT UNSIGNED                 NOT NULL,
+    `comment_uid`  INT UNSIGNED                 NOT NULL,
+    `moment_uid`   INT UNSIGNED                 NOT NULL,
+    `like_count`   INT UNSIGNED                 NOT NULL,
+    `comment_time` DATETIME                     NOT NULL DEFAULT NOW()
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 ALTER TABLE `comment`
-    ADD CONSTRAINT fk_comment_moment FOREIGN KEY (`mid`) REFERENCES `moment` (`mid`);
+    ADD CONSTRAINT fk_comment_moment FOREIGN KEY (`moment_id`) REFERENCES `moment` (`moment_id`);
 ALTER TABLE `comment`
-    ADD CONSTRAINT fk_comment_user FOREIGN KEY (`cuid`) REFERENCES `user` (`uid`);
+    ADD CONSTRAINT fk_comment_user FOREIGN KEY (`comment_uid`) REFERENCES `user` (`uid`);
 
 CREATE TABLE IF NOT EXISTS `like`
 (
-    `lid`  INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `mid`  INT UNSIGNED NOT NULL,
-    `luid` INT UNSIGNED NOT NULL,
-    `muid` INT UNSIGNED NOT NULL
+    `like_id`    INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `moment_id`  INT UNSIGNED NOT NULL,
+    `like_uid`   INT UNSIGNED NOT NULL,
+    `moment_uid` INT UNSIGNED NOT NULL,
+    `like_time`  DATETIME     NOT NULL DEFAULT NOW()
 ) ENGINE = InnoDB;
 ALTER TABLE `like`
-    ADD CONSTRAINT fk_like_moment FOREIGN KEY (`mid`) REFERENCES `moment` (`mid`);
+    ADD CONSTRAINT fk_like_moment FOREIGN KEY (`moment_id`) REFERENCES `moment` (`moment_id`);
 ALTER TABLE `like`
-    ADD CONSTRAINT fk_like_user FOREIGN KEY (`luid`) REFERENCES `user` (`uid`);
+    ADD CONSTRAINT fk_like_user FOREIGN KEY (`like_uid`) REFERENCES `user` (`uid`);
 
 CREATE TABLE IF NOT EXISTS `repost`
 (
-    `rid`   INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `mid`   INT UNSIGNED NOT NULL,
-    `ruid`  INT UNSIGNED NOT NULL,
-    `muid`  INT UNSIGNED NOT NULL,
-    `rtime` DATETIME     NOT NULL DEFAULT NOW()
+    `repost_id`   INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `moment_id`   INT UNSIGNED NOT NULL,
+    `repost_uid`  INT UNSIGNED NOT NULL,
+    `moment_uid`  INT UNSIGNED NOT NULL,
+    `repost_time` DATETIME     NOT NULL DEFAULT NOW()
 ) ENGINE = InnoDB;
 ALTER TABLE `repost`
-    ADD CONSTRAINT fk_repost_moment FOREIGN KEY (`mid`) REFERENCES `moment` (`mid`);
+    ADD CONSTRAINT fk_repost_moment FOREIGN KEY (`moment_id`) REFERENCES `moment` (`moment_id`);
 ALTER TABLE `repost`
-    ADD CONSTRAINT fk_repost_user FOREIGN KEY (`ruid`) REFERENCES `user` (`uid`);
+    ADD CONSTRAINT fk_repost_user FOREIGN KEY (`repost_uid`) REFERENCES `user` (`uid`);
 
 CREATE TABLE IF NOT EXISTS `star`
 (
-    `sid`   INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `mid`   INT UNSIGNED NOT NULL,
-    `suid`  INT UNSIGNED NOT NULL,
-    `stime` DATETIME     NOT NULL DEFAULT NOW()
+    `star_id`   INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `moment_id` INT UNSIGNED NOT NULL,
+    `star_uid`  INT UNSIGNED NOT NULL,
+    `star_time` DATETIME     NOT NULL DEFAULT NOW()
 ) ENGINE = InnoDB;
 ALTER TABLE `star`
-    ADD CONSTRAINT fk_star_moment FOREIGN KEY (`mid`) REFERENCES `moment` (`mid`);
+    ADD CONSTRAINT fk_star_moment FOREIGN KEY (`moment_id`) REFERENCES `moment` (`moment_id`);
 ALTER TABLE `star`
-    ADD CONSTRAINT fk_star_user FOREIGN KEY (`suid`) REFERENCES `user` (`uid`);
+    ADD CONSTRAINT fk_star_user FOREIGN KEY (`star_uid`) REFERENCES `user` (`uid`);
 
 CREATE TABLE IF NOT EXISTS `user_info`
 (
     `uid`          INT UNSIGNED                NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `username`     VARCHAR(30) CHARSET utf8mb4 NOT NULL,
-    `urealname`    VARCHAR(30) CHARSET utf8mb4,
-    `uavatarurl`   VARCHAR(255),
-    `uabout`       VARCHAR(255) CHARSET utf8mb4,
-    `ufollowing`   INT,
-    `ufollowers`   INT,
-    `umomentcount` INT,
-    `ulikecount`   INT,
-    `ustarcount`   INT,
-    `ubirthday`    DATE
+    `real_name`    VARCHAR(30) CHARSET utf8mb4,
+    `avatar_url`   VARCHAR(255),
+    `about`        VARCHAR(255) CHARSET utf8mb4,
+    `phone`        VARCHAR(30),
+    `email`        VARCHAR(50),
+    `birthday`     DATE,
+    `following`    INT,
+    `followers`    INT,
+    `moment_count` INT,
+    `like_count`   INT,
+    `star_count`   INT
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 ALTER TABLE `user_info`
