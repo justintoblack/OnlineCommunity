fe24383c9d48dd143f819a71cc2bc4d121a73200
ymc <1518805452@qq.com>
2022-06-08 16:33:38 +0800

    登录token

--- src/main/java/com/onlinecommunity/SpringbootApplication.java
+++ src/main/java/com/onlinecommunity/SpringbootApplication.java
@@ -1,13 +1,17 @@
 package com.onlinecommunity;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class SpringbootApplication {
+public class JavaeeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+
+        SpringApplication.run(JavaeeApplication.class, args);
+        log.info("项目启动成功。。。");
     }
 
 }
