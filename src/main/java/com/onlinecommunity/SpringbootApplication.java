c0e525890266c3ff9e87c28d24820c6558ac4984
曾杰航 <870582311@qq.com>
2022-06-27 19:40:07 +0800

    设置了需要登录才能访问图片，修改了使其可以最终打包

--- src/main/java/com/onlinecommunity/SpringbootApplication.java
+++ src/main/java/com/onlinecommunity/SpringbootApplication.java
@@ -3,15 +3,28 @@ package com.onlinecommunity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @Slf4j
+//@SpringBootApplication
+//public class SpringbootApplication {
+//
+//    public static void main(String[] args) {
+//
+//        SpringApplication.run(SpringbootApplication.class, args);
+//        log.info("项目启动成功。。。");
+//    }
+//
+//}
 @SpringBootApplication
-public class SpringbootApplication {
-
-    public static void main(String[] args) {
-
-        SpringApplication.run(SpringbootApplication.class, args);
-        log.info("项目启动成功。。。");
+public class SpringbootApplication extends SpringBootServletInitializer{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootApplication.class);
     }
 
+    public static void main(String[] args){
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
 }
--- src/main/java/com/onlinecommunity/config/AdminWebConfig.java
+++ src/main/java/com/onlinecommunity/config/AdminWebConfig.java
@@ -17,7 +17,7 @@ public class AdminWebConfig implements WebMvcConfigurer {
 
         registry.addInterceptor(new LoginHandlerInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/","/login","/register","/static/**","/pic/**");
+                .excludePathPatterns("/","/login","/register");
 
     }
 
